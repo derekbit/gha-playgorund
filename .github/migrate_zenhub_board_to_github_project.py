e655444aca0cb36dfe6d14b1c5ae2a5790a3c571
Derek Su <derek.su@suse.com>
2024-08-01 01:46:10 +0800

    WIP
    Signed-off-by: Derek Su <derek.su@suse.com>

--- .github/migrate_zenhub_board_to_github_project.py
+++ .github/migrate_zenhub_board_to_github_project.py
@@ -2,16 +2,14 @@ import requests
 import os
 import jq
 
-GITHUB_ORG = "dereksu-org"
-GITHUB_REPO = "gha-playground"
-GITHUB_PROJECT = "helloworld"
+
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_GRAPHQL_URL = "https://api.github.com/graphql"
-
 ZENHUB_API_URL = "https://api.zenhub.com/p1/repositories/{repo_id}/board"
 
-def get_github_repo_id(github_token):
-    url = f"{GITHUB_API_URL}/repos/{GITHUB_ORG}/{GITHUB_REPO}"
+
+def get_github_repo_id(github_token, github_org, github_repo):
+    url = f"{GITHUB_API_URL}/repos/{github_org}/{github_repo}"
     headers = {
         "Authorization": github_token
     }
@@ -24,8 +22,8 @@ def get_github_repo_id(github_token):
         response.raise_for_status()
 
 
-def get_zenhub_board(zenhub_token, github_token):
-    url = ZENHUB_API_URL.format(repo_id=get_github_repo_id(github_token))
+def get_zenhub_board(zenhub_token, github_repo_id):
+    url = ZENHUB_API_URL.format(repo_id=github_repo_id)
     headers = {
         "Content-Type": "application/json",
         "X-Authentication-Token": zenhub_token
@@ -152,7 +150,8 @@ def add_github_project_item(github_token, project_id, content_id):
     return response.json()
 
 
-def move_item_to_status(github_token, project_id, item_id, field_id, single_select_option_id):
+def move_item_to_status(github_token, project_id, item_id, field_id,
+                        single_select_option_id):
     headers = {
         "Authorization": f"Bearer {github_token}",
         "Content-Type": "application/json"
@@ -180,10 +179,9 @@ def move_item_to_status(github_token, project_id, item_id, field_id, single_sele
 def migrate_tickets():
     github_token = os.getenv("GITHUB_TOKEN")
     zenhub_token = os.getenv("ZENHUB_ACCESS_TOKEN")
-
-    github_org = "dereksu-org"
-    github_repo = "gha-playground"
-    github_project = "helloworld"
+    github_org = os.getenv("GITHUB_ORG")
+    github_repo = os.getenv("GITHUB_REPO")
+    github_project = os.getenv("GITHUB_PROJECT")
 
     # Get the GitHub Project details
     project = get_github_project(github_token, github_org, github_project)
@@ -194,7 +192,8 @@ def migrate_tickets():
     print(f"GitHub Project Details: number={project_number}, id={project_id}, status={status}")
 
     # Get the ZenHub board details
-    board = get_zenhub_board(zenhub_token, github_token)
+    github_repo_id = get_github_repo_id(github_token, github_org, github_repo)
+    board = get_zenhub_board(zenhub_token, github_token, github_repo_id)
     for pipeline in board['pipelines']:
         # Iterating through each pipeline, which are corresponding to the GitHub Project statuses (columns)
         column_name = pipeline['name']
