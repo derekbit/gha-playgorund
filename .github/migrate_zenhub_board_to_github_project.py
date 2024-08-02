6382cb0d9254364c988683221d9c13a3f6d5f2bb
Derek Su <derek.su@suse.com>
2024-08-02 12:18:45 +0800

    WIP
    Signed-off-by: Derek Su <derek.su@suse.com>

--- .github/migrate_zenhub_board_to_github_project.py
+++ .github/migrate_zenhub_board_to_github_project.py
@@ -264,7 +264,7 @@ def migrate_tickets(github_org, github_repo, github_project):
                                 status[column_name])
             set_item_estimate(github_token,
                               project_id, item_id,
-                              estimate_node_id, 3)
+                              estimate_node_id, issue['estimate'])
 
 
 if __name__ == "__main__":
