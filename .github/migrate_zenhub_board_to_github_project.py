2aacb7d958e754b2e7e7cacc4dccf27cd53bccce
Derek Su <derek.su@suse.com>
2024-08-02 12:06:43 +0800

    WIP
    Signed-off-by: Derek Su <derek.su@suse.com>

--- .github/migrate_zenhub_board_to_github_project.py
+++ .github/migrate_zenhub_board_to_github_project.py
@@ -235,7 +235,7 @@ def migrate_tickets(github_org, github_repo, github_project):
     print(f"GitHub Project Details: number={project_number}, id={project_id}, status node_id={status_node_id}, status={status}")
 
     estimate_node_id = get_github_project_status(github_token, github_org, github_repo, project_number)
-    print(f"GitHub Project Details: number={project_number}, id={project_id}, estimate node_id={estimate_node_id})
+    print(f"GitHub Project Details: number={project_number}, id={project_id}, estimate node_id={estimate_node_id}")
 
     # Get the ZenHub board details
     github_repo_id = get_github_repo_id(github_token, github_org, github_repo)
